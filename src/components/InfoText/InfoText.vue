b0287e1ff4bdb567bcac7e57f516fc0eb2d632d8
Pavel Spacil <pavel.spacil@gmail.com>
2023-03-14 07:54:05 +0100

    chore: adjust text

--- src/components/InfoText/InfoText.vue
+++ src/components/InfoText/InfoText.vue
@@ -2,7 +2,8 @@
     <div class="infotext">
         <p>
             Fantom on Fire monitors the total amount of FTM burnt, <span class="number">70%</span> of the fees paid to
-            the Opera network go to validators, the remaining <span class="number">20%</span> are burnt forever.
+            the Opera network go to validators, <span class="number">10%</span> is allocated to the Ecosystem Vault and
+            the remaining <span class="number">20%</span> are burnt forever.
         </p>
     </div>
 </template>
